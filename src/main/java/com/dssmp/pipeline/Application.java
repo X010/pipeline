d2f20972b6e096bdbcd4ca594743c475449f98c7
x010 <jeffrey@JeffreydeMacBook-Pro.local>
2016-03-27 22:06:13 +0800

    init

--- src/main/java/com/dssmp/pipeline/Application.java
+++ src/main/java/com/dssmp/pipeline/Application.java
@@ -2,12 +2,11 @@ package com.dssmp.pipeline;
 
 /**
  * Hello world!
- *
  */
-public class Application
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class Application {
+
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
 }
